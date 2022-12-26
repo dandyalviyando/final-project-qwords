a20224076a2050b568100e037abbe03de55416f7
dandy.alv <dandy.alv@gmail.com>
2022-12-26 18:17:04 +0800

    membuat section cloud hosting dan vps

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -24,5 +24,94 @@
     </div>
 @endsection
 @section('content')
+    <section id="pricing" class="pricing">
+        <div class="container" data-aos="fade-up">
+          <div class="section-title">
+            <h3>Cloud Hosting dan VPS <span>Diskon Hingga 60% !</span></h3>
+            <p>Layanan unggulan dengan teknologi terbaik untuk website anda.</p>
+          </div>
+          <div class="row">
+            <div class="col-lg-3 col-md-6 " data-aos="fade-up" data-aos-delay="100">
+              <div class="box featured1 card-cloud">
+                <h3>VCH1</h3>
+                <img src="{{asset ('assets/img/pricing1.png') }}" alt="pricing-1" class="mb-4" style="width: 150px; height: 150px;">
+                <h3>Value Cloud Hosting</h3>
+                <div style="color: #777777; text-decoration:line-through;"> Rp. 24.500</div>
+                <h4><sup>Rp</sup>14.500</h4>
+                <ul>
+                  <li>3 GB Disk</li>
+                  <li>Unlimited Bandwidth</li>
+                  <li>0.5 Core CPU</li>
+                  <li class="na">Addon Domain</li>
+                  <li class="na">Parked Domain</li>
+                </ul>
+                <div class="btn-wrap">
+                  <a href="#" class="btn-buy">Order sekarang</a>
+                </div>
+              </div>
+            </div>
+            <div class="col-lg-3 col-md-6 mt-4 mt-md-0 " data-aos="fade-up" data-aos-delay="200">
+                <div class="box featured2 card-cloud">
+                  <h3>Signature</h3>
+                  <img src="{{asset ('assets/img/pricing2.png') }}" alt="pricing-2" class="mb-4" style="width: 150px; height: 150px;">
+                  <h3>Unlimited Hosting</h3>
+                  <div style="color: #777777; text-decoration:line-through;"> Rp. 109.000</div>
+                  <h4><sup>Rp</sup>84.900</h4>
+                  <ul>
+                    <li>3 GB Disk</li>
+                    <li>Unlimited Bandwidth</li>
+                    <li>0.5 Core CPU</li>
+                    <li>5 Addon Domain</li>
+                    <li>5 Parked Domain</li>
+                    
+                  </ul>
+                  <div class="btn-wrap">
+                    <a href="#" class="btn-buy">Order sekarang</a>
+                  </div>
+                </div>
+            </div>
+            <div class="col-lg-3 col-md-6 mt-4 mt-md-0 " data-aos="fade-up" data-aos-delay="200">
+                <div class="box featured3 card-cloud">
+                  <h3>HPCH Bisnis 1</h3>
+                  <img src="{{asset ('assets/img/pricing3.png') }}" alt="pricing-3" class="mb-4" style="width: 150px; height: 150px;">
+                  <h3>High Performance</h3>
+                  <div style="color: #777777; text-decoration:line-through;"> Rp. 80.000</div>
+                  <h4><sup>Rp</sup>69.000</h4>
+                  <ul>
+                    <li>3 GB Disk</li>
+                    <li>Unlimited Bandwidth</li>
+                    <li>1 Core CPU</li>
+                    <li class="na">5 Addon Domain</li>
+                    <li class="na">5 Parked Domain</li>
+                    
+                  </ul>
+                  <div class="btn-wrap">
+                    <a href="#" class="btn-buy">Order sekarang</a>
+                  </div>
+                </div>
+            </div>
+            <div class="col-lg-3 col-md-6 mt-4 mt-md-0 " data-aos="fade-up" data-aos-delay="200">
+                <div class="box featured4 card-cloud">
+                  <h3>VPS SC1</h3>
+                  <img src="{{asset ('assets/img/pricing4.png') }}" alt="pricing-4" class="mb-4" style="width: 150px; height: 150px;">
+                  <h3>Cloud VPS KVM SSD</h3>
+                  <div style="color: #777777; text-decoration:line-through;"> Rp. 135.000</div>
+                  <h4><sup>Rp</sup>75.000</h4>
+                  <ul>
+                    <li>25 GB Disk</li>
+                    <li>Unlimited Bandwidth</li>
+                    <li>1 Core CPU</li>
+                    <li>Unlimited Addon Domain</li>
+                    <li>Unlimited Parked Domain</li>
+                    
+                  </ul>
+                  <div class="btn-wrap">
+                    <a href="#" class="btn-buy">Order sekarang</a>
+                  </div>
+                </div>
+            </div>
+          </div>
+        </div>
+      </section>
 @endsection
  
